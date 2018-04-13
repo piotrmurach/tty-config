4aa2218d4b257e01d7bf50a5a5b51dc37e0ab16f
Piotr Murach <pmurach@gmail.com>
2018-04-13 22:47:37 +0100

    Fix spec to work on older rubies

--- spec/unit/normalize_hash_spec.rb
+++ spec/unit/normalize_hash_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe TTY::Config, '#normalize_hash' do
   it "normalizes keys via method to symbols" do
     hash = {
-      "settings": {
-        "base": "USD",
-        "color": true,
-        "exchange": "CCCAGG"
+      "settings" => {
+        "base" => "USD",
+        "color" => true,
+        "exchange" => "CCCAGG"
       },
-      "coins": ["BTC", "ETH", "TRX", "DASH"]
+      "coins" => ["BTC", "ETH", "TRX", "DASH"]
     }
 
     expect(TTY::Config.normalize_hash(hash)).to eq({

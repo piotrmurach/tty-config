c454382aa6e104fb306f427615994631340bab05
Piotr Murach <pmurach@gmail.com>
2018-04-14 11:42:34 +0100

    Change tests to ensure return values

--- spec/unit/delete_spec.rb
+++ spec/unit/delete_spec.rb
@@ -2,14 +2,21 @@ RSpec.describe TTY::Config, '#delete' do
   it "deletes the value" do
     config = TTY::Config.new
     config.set(:foo, value: 2)
-    config.delete(:foo)
+    expect(config.delete(:foo)).to eq(2)
     expect(config.fetch(:foo)).to eq(nil)
   end
 
   it "deletes the value under deeply nested key" do
     config = TTY::Config.new
     config.set(:foo, :bar, :baz) { 2 }
-    config.delete(:foo, :bar, :baz)
+    expect(config.delete(:foo, :bar, :baz).call).to eq(2)
     expect(config.fetch(:foo, :bar, :baz)).to eq(nil)
+  end
+
+  it "deletes innermost key with array value" do
+    config = TTY::Config.new
+    config.set(:foo, :bar, value: [1,2,3])
+    expect(config.delete(:foo, :bar)).to eq([1,2,3])
+    expect(config.fetch(:foo, :bar)).to eq(nil)
   end
 end

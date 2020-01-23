52ee44066a6a2b18d56429a5e3160f3b50a982f9
Piotr Murach <pmurach@gmail.com>
2020-01-23 22:51:12 +0100

    Add perf test for reading and writing configs

---
+++ spec/perf/read_write_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rspec-benchmark"
+require "yaml/store"
+
+RSpec.describe TTY::Config do
+  include RSpec::Benchmark::Matchers
+
+  it "reads and writes keys at most 1.5x slower than built-in yaml store" do
+    config_file = tmp_path("config.yml")
+    config = TTY::Config.new
+
+    store_file = tmp_path("store.yml")
+    store = YAML::Store.new(store_file)
+
+    expect {
+      config.read(config_file) if config.exist?
+      config.set(:foo, value: "bar")
+      config.write
+    }.to perform_slower_than {
+      store.transaction { store[:foo] = "bar" }
+    }.at_most(1.5).times
+  end
+end
